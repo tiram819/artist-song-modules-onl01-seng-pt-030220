4a6a0c01ca66cf8987e724bdd4f1d6d691cdfbea
Yeasha Hassan <tiram819@gmail.com>
2020-04-03 20:58:59 +0000

    Automatically backed up by Learn

--- lib/concers/memorable.rb
+++ lib/concers/memorable.rb
@@ -1,47 +1,4 @@
 module Memorable
-  module ClassMethods
-    def reset_all
-      self.all.clear
-    end
-
-    def count
-      self.all.count
-    end
-  end
-  
-  module InstanceMethods
-    def initialize
-      self.class.all << self
-    end
-  end
-
-class Artist
-  attr_accessor :name
-  attr_reader :songs
-
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
-
-  @@artists = []
-  
-  def self.all
-    @@artists
-  end
-
-  def initialize
-    super
-    @songs = []
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self unless song.artist
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
+ 
 
 end
